e4589ce41211a6870de793895eb7eea5850c304f
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-12-04 13:04:07 -0700

    feat(api): validate and use local storage cache

--- src/api.js
+++ src/api.js
@@ -1,46 +1,44 @@
 import { Result, Validation } from "./utils";
 
-const PRODUCT_DATA_URL =
-  "https://gist.githubusercontent.com/rconnolly/d37a491b50203d66d043c26f33dbd798/raw/37b5b68c527ddbe824eaed12073d266d5455432a/clothing-compact.json";
-
 const ProductAPI = {
   async fetch() {
-    return await fetch(PRODUCT_DATA_URL)
+    const VALIDATION_NAME = "product-fetch";
+    const LOCAL_STORAGE_KEY = "products";
+    const FETCH_URL =
+      "https://gist.githubusercontent.com/rconnolly/d37a491b50203d66d043c26f33dbd798/raw/37b5b68c527ddbe824eaed12073d266d5455432a/clothing-compact.json";
+
+    const cache = Validation.getByLocalStorage(
+      VALIDATION_NAME,
+      LOCAL_STORAGE_KEY,
+    );
+
+    if (cache.ok) {
+      return cache;
+    }
+
+    return await fetch(FETCH_URL)
       .then((response) => {
         if (response.ok) {
           return Result.ok(response);
         } else {
-          return Result.error("Failed to fetch product data", response);
+          return Result.error("Failed to fetch products", response);
         }
       })
-      .then((response) => Result.ok(response.json()))
-      .catch((error) => Result.error("Non-JSON format found", error));
+      .then(
+        (response) => {
+          const result = Validation.getArray(response.json());
+
+          if (result.ok) {
+            return Validation.setByLocalStorage(
+              VALIDATION_NAME,
+              LOCAL_STORAGE_KEY,
+              result.data,
+            );
+          }
+
+          return result;
+        },
+        (error) => Result.error("Non-JSON format found", error),
+      );
   },
 };
-
-function validate(json) {
-  if (Object.keys(json).length === 0) {
-    return Result.error("Empty object found", json);
-  }
-
-  return {
-    name() {
-      return Validation.getString("name", json.name);
-    },
-    gender() {
-      return Validation.getString("gender", json.gender);
-    },
-    category() {
-      return Validation.getString("category", json.category);
-    },
-    description() {
-      return Validation.getString("description", json.description);
-    },
-    price() {
-      return Validation.getNumber("price", json.price);
-    },
-    shipping() {
-      return Validation.getNumber("cost", json.cost);
-    },
-  };
-}
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,42 +1,108 @@
 import { Result } from "index";
 
 export const Validation = {
-  getArray(name, value) {
-    if (!name) {
-      return Result.error("No name found to validate for type array", {
-        name,
-        value,
-      });
-    } else if (!Array.isArray(value)) {
-      return Result.error(`Non-array '${name}' found`, { name, value });
-    } else if (value.length === 0) {
-      return Result.error(`Empty array of '${name}' found`, { name, value });
-    } else {
-      return Result.ok(value);
+  isObjectEmpty(name, value) {
+    const result = present(name, value, "object");
+
+    if (result.ok) {
+      if (Object.keys(value).length === 0) {
+        return Result.error(`Empty '${name}' object found`, { name, value });
+      } else {
+        return Result.ok(value);
+      }
     }
+
+    return result; // propagate error
+  },
+  getByLocalStorage(name, key) {
+    const result = present(name, key, "local-storage key");
+
+    if (result.ok) {
+      const value = localStorage.getItem(key);
+
+      if (value) {
+        return Result.ok(value);
+      } else {
+        return Result.error(`No '${key}' from local-storage found`, {
+          name,
+          key,
+        });
+      }
+    }
+
+    return result; // propagate error
+  },
+  setByLocalStorage(name, key, value) {
+    const rkey = present(name, key, "local-storage key");
+    const rvalue = present(name, key, "local-storage value");
+
+    if (!rkey.ok) {
+      return rkey; // propagate error
+    } else if (!rvalue.ok) {
+      return rvalue; // propagate error
+    } else {
+      try {
+        localStorage.setItem(key, value);
+      } catch (error) {
+        return Result.error(
+          `Failed to update '${key}' for '${name}' from local-storage`,
+          { name, key, value, error },
+        );
+      }
+
+      return Result.ok([rkey, rvalue]);
+    }
+  },
+  getArray(name, value) {
+    const result = present(name, value, "array");
+
+    if (result.ok) {
+      if (!Array.isArray(value)) {
+        return Result.error(`Non-array '${name}' found`, { name, value });
+      } else if (value.length === 0) {
+        return Result.error(`Empty array of '${name}' found`, { name, value });
+      } else {
+        return Result.ok(value);
+      }
+    }
+
+    return result; // propagate error
   },
   getNumber(name, value) {
-    if (!name) {
-      return Result.error("No name found to validate for type number", {
-        name,
-        value,
-      });
-    } else if (!(typeof value === "number")) {
-      return Result.error(`Non-number '${name}' found`, { name, value });
-    } else {
-      return Result.ok(value);
+    const result = present(name, value, "number");
+
+    if (result.ok) {
+      if (!(typeof value === "number")) {
+        return Result.error(`Non-number '${name}' found`, { name, value });
+      } else {
+        return Result.ok(value);
+      }
     }
+
+    return result; // propagate error
   },
   getString(name, value) {
-    if (!name) {
-      return Result.error("No name found to validate for type string", {
-        name,
-        value,
-      });
-    } else if (!(typeof value === "string")) {
-      return Result.error(`Non-string '${name}' found`, { name, value });
-    } else {
-      return Result.ok(value);
+    const result = present(name, value, "string");
+
+    if (result.ok) {
+      if (!(typeof value === "string")) {
+        return Result.error(`Non-string '${name}' found`, { name, value });
+      } else {
+        return Result.ok(value);
+      }
     }
+
+    return result; // propagate error
   },
 };
+
+function present(name, value, type) {
+  if (!name) {
+    return Result.error(`No name found to validate for type ${type}`, {
+      name,
+      value,
+    });
+  } else if (!value) {
+    return Result.error(`No '${name}' ${type} found`, { name, value });
+  }
+}
