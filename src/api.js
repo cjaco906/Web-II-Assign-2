8f80f3713f0a973f1fdf0234dd7ce971e1b35b8b
ramos <113315631+cjaco906@users.noreply.github.com>
2025-12-06 16:28:13 -0700

    feat(browsing): add recommendations

--- src/api.js
+++ src/api.js
@@ -55,3 +55,81 @@ export const ProductStorage = {
       );
   },
 };
+
+export const ProductBrowsing = {
+  getScore(base, target) {
+    const [rbase, rtarget] = [
+      Validation.getObject(base),
+      Validation.getObject(target),
+    ];
+
+    if (!rbase.ok) {
+      return rbase;
+    } else if (!rtarget.ok) {
+      return rtarget;
+    } else {
+      const [{ data: base }, { data: target }] = [rbase, rtarget];
+      let score = 0;
+
+      for (const [key, bvalue] of Object.entries(base)) {
+        const tvalue = target[key];
+        const isObject = typeof bvalue === "object";
+
+        if (isObject) {
+          const subscore = this.getScore(bvalue, tvalue);
+
+          if (subscore.ok) {
+            score += subscore.data.score;
+          } else {
+            return subscore;
+          }
+        }
+
+        if (bvalue === tvalue) {
+          ++score;
+        }
+      }
+
+      return Result.ok({ score, product: target });
+    }
+  },
+  async getRecommendations(base, limit) {
+    if (!base) {
+      return Result.error("No product found for recommendations", base);
+    }
+
+    const products = await ProductStorage.fetch();
+
+    if (products.ok) {
+      const scores = [];
+
+      for (const target of products.data) {
+        if (base.id === target.id) continue;
+
+        const check = Validation.getObject(target);
+
+        if (check.ok) {
+          const score = this.getScore(base, target);
+
+          if (score.ok) {
+            scores.push(score.data);
+          } else {
+            return score;
+          }
+        } else {
+          return check;
+        }
+      }
+
+      const sorted = scores.sort((a, b) => b.score - a.score);
+
+      if (limit) {
+        sorted.length = limit;
+      }
+
+      return Result.ok(sorted);
+    }
+
+    return products;
+  },
+};
