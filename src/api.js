97f95142299a7babb7e25e41e191b284d147e0d4
ramos <113315631+cjaco906@users.noreply.github.com>
2025-12-06 14:45:53 -0700

    feat(api): finalize product fetching

--- src/api.js
+++ src/api.js
@@ -1,42 +1,55 @@
 import { Result, Validation } from "./utils";
 
-const ProductAPI = {
+export const ProductStorage = {
   async fetch() {
-    const VALIDATION_NAME = "product-fetch";
     const LOCAL_STORAGE_KEY = "products";
     const FETCH_URL =
       "https://gist.githubusercontent.com/rconnolly/d37a491b50203d66d043c26f33dbd798/raw/37b5b68c527ddbe824eaed12073d266d5455432a/clothing-compact.json";
 
-    const cache = Validation.getByLocalStorage(
-      VALIDATION_NAME,
-      LOCAL_STORAGE_KEY,
-    );
+    const cache = Validation.getByLocalStorage(LOCAL_STORAGE_KEY);
 
     if (cache.ok) {
-      return cache;
+      try {
+        return Result.ok(cache.data);
+      } catch (error) {
+        return Result.error("Failed to parse the products cache", {
+          cache,
+          error,
+        });
+      }
     }
 
     return await fetch(FETCH_URL)
       .then((response) => {
         if (response.ok) {
-          return Result.ok(response);
+          return Result.ok(response.json());
         } else {
           return Result.error("Failed to fetch products", response);
         }
       })
       .then(
-        (response) => {
-          const result = Validation.getArray(response.json());
+        async (response) => {
+          if (response.ok) {
+            const products = Validation.getArray(await response.data);
 
-          if (result.ok) {
-            return Validation.setByLocalStorage(
-              VALIDATION_NAME,
-              LOCAL_STORAGE_KEY,
-              result.data,
-            );
+            if (products.ok) {
+              try {
+                return Validation.setByLocalStorage(
+                  LOCAL_STORAGE_KEY,
+                  JSON.stringify(products.data),
+                );
+              } catch (error) {
+                return Result.error(
+                  "Failed to stringify the downloaded products",
+                  { products, error },
+                );
+              }
+            }
+
+            return products;
+          } else {
+            return response;
           }
-
-          return result;
         },
         (error) => Result.error("Non-JSON format found", error),
       );
