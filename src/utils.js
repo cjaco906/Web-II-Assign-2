ab4d95753889f944b6364186a860357bf9383038
ramos <113315631+cjaco906@users.noreply.github.com>
2025-12-06 16:27:46 -0700

    feat: update utils

--- src/utils.js
+++ src/utils.js
@@ -16,7 +16,7 @@ export const Result = {
     const result = {
       ok: false,
       data: null,
-      error: new Error(message, cause),
+      error: new Error(message, { cause }),
     };
 
     console.error(
@@ -24,8 +24,7 @@ export const Result = {
       "MESSAGE",
       message + "\n",
       "CAUSE",
-      cause + "\n",
-      result,
+      JSON.stringify(cause) + "\n",
     );
 
     return result;
@@ -37,132 +36,90 @@ export const Result = {
       error: null,
     };
   },
-  short(result) {
-    return {
-      success(callback) {
-        if (result.ok) {
-          callback(result.data);
-        }
-
-        return result;
-      },
-      error(callback) {
-        if (result.error) {
-          callback(result.error);
-        }
-
-        return result;
-      },
-    };
-  },
 };
 
 export const Validation = {
-  isObjectEmpty(name, value) {
-    const result = present(name, value, "object");
-
-    if (result.ok) {
-      if (Object.keys(value).length === 0) {
-        return Result.error(`Empty '${name}' object found`, { name, value });
-      } else {
-        return Result.ok(value);
-      }
-    }
-
-    return result; // propagate error
+  getStringOrNumber(value) {
+    return typeof value === "number"
+      ? this.getNumber(value)
+      : this.getString(value);
   },
-  getByLocalStorage(name, key) {
-    const result = present(name, key, "local-storage key");
-
-    if (result.ok) {
+  getObject(value) {
+    if (!value) {
+      return Result.error("No object found", value);
+    } else if (!(typeof value === "object")) {
+      return Result.error("Non-object found", value);
+    } else if (Object.keys(value).length === 0) {
+      return Result.error(`Empty object found`, value);
+    } else {
+      return Result.ok(value);
+    }
+  },
+  getByLocalStorage(key) {
+    if (!key) {
+      return Result.error("No key found for local-storage", key);
+    } else {
       const value = localStorage.getItem(key);
 
       if (value) {
         return Result.ok(value);
       } else {
-        return Result.error(`No '${key}' from local-storage found`, {
-          name,
+        return Result.error(
+          `No value for '${key}' from local-storage found`,
           key,
-        });
+        );
       }
     }
-
-    return result; // propagate error
   },
-  setByLocalStorage(name, key, value) {
-    const rkey = present(name, key, "local-storage key");
-    const rvalue = present(name, key, "local-storage value");
-
-    if (!rkey.ok) {
-      return rkey; // propagate error
-    } else if (!rvalue.ok) {
-      return rvalue; // propagate error
+  setByLocalStorage(key, value) {
+    if (!key) {
+      return Result.error("No key found for local-storage", { key, value });
+    } else if (!value) {
+      return Result.error("No value found for local-storage", { key, value });
     } else {
       try {
         localStorage.setItem(key, value);
       } catch (error) {
-        return Result.error(
-          `Failed to update '${key}' for '${name}' from local-storage`,
-          { name, key, value, error },
-        );
+        return Result.error(`Failed to update '${key}' from local-storage`, {
+          key,
+          value,
+          error,
+        });
       }
 
-      return Result.ok([rkey, rvalue]);
+      return Result.ok(value);
     }
   },
-  getArray(name, value) {
-    const result = present(name, value, "array");
-
-    if (result.ok) {
-      if (!Array.isArray(value)) {
-        return Result.error(`Non-array '${name}' found`, { name, value });
-      } else if (value.length === 0) {
-        return Result.error(`Empty array of '${name}' found`, { name, value });
-      } else {
-        return Result.ok(value);
-      }
+  getArray(value) {
+    if (!value) {
+      return Result.error("No array found", value);
+    } else if (!Array.isArray(value)) {
+      return Result.error(`Non-array found`, value);
+    } else if (value.length === 0) {
+      return Result.error(`Empty array found`, value);
+    } else {
+      return Result.ok(value);
     }
-
-    return result; // propagate error
   },
-  getNumber(name, value) {
-    const result = present(name, value, "number");
-
-    if (result.ok) {
-      if (!(typeof value === "number")) {
-        return Result.error(`Non-number '${name}' found`, { name, value });
-      } else {
-        return Result.ok(+value);
-      }
+  getNumber(value) {
+    if (!value) {
+      return Result.error("No number found", value);
+    } else if (!(typeof +value === "number")) {
+      return Result.error(`Non-number value found`, value);
+    } else {
+      return Result.ok(+value);
     }
-
-    return result; // propagate error
   },
-  getString(name, value) {
-    const result = present(name, value, "string");
-
-    if (result.ok) {
-      if (!(typeof value === "string")) {
-        return Result.error(`Non-string '${name}' found`, { name, value });
-      } else {
-        return Result.ok(value);
-      }
+  getString(value) {
+    if (!value) {
+      return Result.error("No string found", value);
+    } else if (!(typeof value === "string")) {
+      return Result.error(`Non-string value found`, value);
+    } else {
+      return Result.ok(value);
     }
-
-    return result; // propagate error
   },
 };
-
-function present(name, value, type) {
-  if (!name) {
-    return Result.error(`No name found to validate for type ${type}`, {
-      name,
-      value,
-    });
-  } else if (!value) {
-    return Result.error(`No '${name}' ${type} found`, { name, value });
-  }
-}
 
 export const UIElements = {
   create(parent, type, callback) {
@@ -170,36 +127,96 @@ export const UIElements = {
     parent.append(element);
 
     if (callback) {
-      callback(element);
+      try {
+        callback(element);
+      } catch (error) {
+        Result.error("Failed to finish callback on element creation", {
+          parent,
+          type,
+          error,
+        });
+      }
     }
 
     return element;
   },
-  getByIds(ids) {
-    return Result.short(
-      ids.map((value) => {
-        const element = document.getElementById(value);
+  // https://developer.mozilla.org/en-US/docs/Web/API/Element/tagName
+  // https://developer.mozilla.org/en-US/docs/Web/API/Element/className
+  renew(id, callback) {
+    const target = document.getElementById(id);
 
-        if (element) {
-          return Result.ok(element);
-        } else {
-          return Result.error("HTML element not found by identifier", value);
+    if (target) {
+      const parent = target.parentElement;
+      const type = target.tagName;
+      const classes = target.className;
+
+      target.remove();
+
+      const change = this.create(parent, type, (renewed) => {
+        renewed.id = id;
+        renewed.className = classes;
+
+        callback(renewed);
+      });
+
+      return Result.ok(change);
+    } else {
+      return Result.error("Invalid element renewal target", { id, target });
+    }
+  },
+  setId(element, id) {
+    if (!element) {
+      return Result.error("No element found", { element, id });
+    } else {
+      element.id = id;
+
+      return Result.ok(element);
+    }
+  },
+  getByIds(ids, callback) {
+    return ids.map((value) => {
+      const element = document.getElementById(value);
+
+      if (element) {
+        try {
+          if (callback) {
+            callback(element);
+          }
+        } catch (error) {
+          return Result.error(
+            "Failed to finish callback on element id retrieval",
+            { value, error },
+          );
         }
-      }),
-    );
+
+        return Result.ok(element);
+      } else {
+        return Result.error("HTML element not found by identifier", value);
+      }
+    });
   },
   getByClasses(classes) {
-    return Result.short(
-      classes.map((value) => {
-        const element = document.getElementsByClassName(value);
+    return classes.map((value) => {
+      const element = document.getElementsByClassName(value);
 
-        if (element) {
-          return Result.ok(element);
-        } else {
-          return Result.error("HTML element not found by class", value);
-        }
-      }),
-    );
+      if (element) {
+        return Result.ok(element);
+      } else {
+        return Result.error("HTML element not found by class", value);
+      }
+    });
+  },
+};
+
+export const UIStyles = {
+  setText(element, value) {
+    const result = Validation.getStringOrNumber(value);
+
+    if (result.ok) {
+      element.textContent = value;
+    }
+
+    return result;
   },
 };
 
@@ -208,24 +225,49 @@ export const UIAttributes = {
     return keys.map((key) => element.getAttribute(key));
   },
   set(element, entries) {
-    entries.forEach(([key, value]) => element.setAttribute(key, value));
+    entries.forEach(([key, value]) => {
+      const rkey = Validation.getString(key);
+      const rvalue = Validation.getStringOrNumber(value);
+
+      if (rkey.ok && rvalue.ok) {
+        element.setAttribute(key, value);
+      }
+    });
   },
   remove(element, keys) {
-    keys.forEach((key) => element.removeAttribute(element, key));
+    keys.forEach((key) => {
+      const result = Validation.getString(key);
+
+      if (result.ok) {
+        element.removeAttribute(element, key);
+      }
+    });
   },
   replace(element, key, [old, change]) {
-    if (element.hasAttribute(key, old)) {
+    const rold = Validation.getStringOrNumber(old);
+    const rchange = Validation.getStringOrNumber(change);
+
+    if (rold.ok && rchange.ok && element.hasAttribute(key, old)) {
       element.setAttribute(key, change);
     }
   },
   toggle(element, keys) {
-    keys.forEach((key) => element.toggleAttribute(key));
+    keys.forEach((key) => {
+      const result = Validation.getString(key);
+
+      if (result.ok) {
+        element.toggleAttribute(key);
+      }
+    });
   },
 };
 
 export const UIClasses = {
   add(element, classes) {
     classes.forEach((value) => element.classList.add(value));
+  },
+  set(element, classes) {
+    element.classList = classes.join(" ");
   },
   remove(element, classes) {
     classes.forEach((value) => element.classList.remove(value));
@@ -235,5 +277,23 @@ export const UIClasses = {
   },
   replace(element, entries) {
     entries.forEach(([old, change]) => element.classList.replace(old, change));
+  },
+};
+
+export const UIEvents = {
+  listen(elements, name, callback) {
+    elements.forEach((element) =>
+      element.addEventListener(name, (event) => {
+        try {
+          callback(event);
+        } catch (error) {
+          Result.error("Failed to finish callback on event", {
+            name,
+            event,
+            error,
+          });
+        }
+      }),
+    );
   },
 };
