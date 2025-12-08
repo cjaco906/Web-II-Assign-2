0207b254e2bb2a0edb751300186826784bb2dbd6
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-12-07 19:14:35 -0700

    feat: add aboutus and browse routes (empty for now)

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,17 @@
-import { UIClasses } from "./utils";
+import { Result, UIClasses, UIElements, UIEvents } from "./utils";
 import { HomeView, ProductView, ShoppingCartView } from "./views";
 
 const Views = {
-  home: HomeView.create("home"),
-  product: ProductView.create("product"),
-  cart: ShoppingCartView.create("shopping-cart"),
+  HOME: HomeView.create("home"),
+  PRODUCT: ProductView.create("product"),
+  CART: ShoppingCartView.create("shopping-cart"),
+};
+
+const NavigationBar = {
+  HOME: UIElements.getByIds(["nav-home"]),
+  HOME_LOGO: UIElements.getByIds(["nav-home-logo"]),
+  BROWSE: UIElements.getByIds(["nav-browse"]),
+  ABOUT_US: UIElements.getByIds(["nav-about"]),
 };
 
 const Router = {
@@ -12,17 +19,39 @@ const Router = {
 };
 
 export const Routes = {
+  init(products) {
+    Result.compute(
+      [...NavigationBar.HOME, ...NavigationBar.HOME_LOGO],
+      ([home, logo]) => {
+        UIEvents.listen([home, logo], "click", () => {
+          this.home(products);
+        });
+      }
+    );
+    Result.compute([...NavigationBar.BROWSE], ([browse]) => {
+      UIEvents.listen([browse], "click", () => {
+        console.log("test");
+      });
+    });
+    Result.compute([...NavigationBar.ABOUT_US], ([aboutus]) => {
+      UIEvents.listen([aboutus], "click", () => {
+        console.log("test");
+      });
+    });
+  },
   home(products) {
     HomeView.update(products);
-    UpdateView.toggle(Views.home);
+    UpdateView.toggle(Views.HOME);
   },
+  aboutus() {},
+  browse() {},
   product(product) {
     ProductView.update(product);
-    UpdateView.toggle(Views.product);
+    UpdateView.toggle(Views.PRODUCT);
   },
   cart(order) {
     ShoppingCartView.update(order);
-    UpdateView.toggle(Views.cart);
+    UpdateView.toggle(Views.CART);
   },
 };
 
