18ec7036560f6526da342d24bec8be9f9e3523cd
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-12-09 10:02:21 -0700

    update build

--- src/main.js
+++ src/main.js
@@ -1,13 +1,5 @@
 import { Routes } from "./routes";
 
-// https://github.com/vitejs/vite/discussions/12191
-// Forces loading all images from deployment
-Object.values(
-  import.meta.glob("/images/*.{png,webp}", {
-    eager: true,
-  }),
-);
-
 /**
  * Visit the home view upon first visit.
  */
