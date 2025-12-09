83746c99499ac0142c4594aaf088f300168900d2
cjaco906 <113315631+cjaco906@users.noreply.github.com>
2025-12-09 09:33:12 -0700

    build: force build to load images

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { Routes } from "./routes";
 
+// https://github.com/vitejs/vite/discussions/12191
+// Forces loading all images from deployment
+Object.values(
+  import.meta.glob("/src/images/*.{png,webp}", { eager: true, as: "url" }),
+);
+
 /**
  * Visit the home view upon first visit.
  */
